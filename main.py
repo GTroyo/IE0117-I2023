8f508a5bf42f28f35263509577664676845972fe
GTroyo <86010462+GTroyo@users.noreply.github.com>
2023-07-01 22:20:54 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#
+# Autor: Gabriel Troyo Barahona-B97893
+# Este código se creó para seguir las instrucciones del
+# lab de git.
+
+if __main__ == '__name__':
+    print("¡Hola Mundo")
